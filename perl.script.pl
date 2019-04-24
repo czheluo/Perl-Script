9dd577880db7fde5dd3fd23c826ddeb0bf9744eb
Chenzhe Luo <czheluo@gmail.com>
2019-04-24 15:44:22 +0800

    Update perl.script.pl

--- perl.script.pl
+++ perl.script.pl
@@ -10,32 +10,50 @@ use File::Basename qw(basename dirname);
 my $version="1.0.0";
 GetOptions(
 	"help|?" =>\&USAGE,
-	#"i:s"=>\$fIn,
-	#"i2:s"=>\$fIn,
-	"i:s"=>\$fin,
-	"o:s"=>\$fout,
-	"m:s"=>\$min,
+	"int:s"=>\$fin,
+	"out:s"=>\$fout,
 			) or &USAGE;
 &USAGE unless ($fout);
+
+
+
 
 #######################################################################################
 print STDOUT "\nDone. Total elapsed time : ",time()-$BEGIN_TIME,"s\n";
 #######################################################################################
+sub ABSOLUTE_DIR #$pavfile=&ABSOLUTE_DIR($pavfile);
+{
+	my $cur_dir=`pwd`;chomp($cur_dir);
+	my ($in)=@_;
+	my $return="";
+	if(-f $in){
+		my $dir=dirname($in);
+		my $file=basename($in);
+		chdir $dir;$dir=`pwd`;chomp $dir;
+		$return="$dir/$file";
+	}elsif(-d $in){
+		chdir $in;$return=`pwd`;chomp $return;
+	}else{
+		warn "Warning just for file and dir \n$in";
+		exit;
+	}
+	chdir $cur_dir;
+	return $return;
+}
 sub USAGE {#
         my $usage=<<"USAGE";
-Contact:        minghao.zhang\@majorbio.com;
+Contact:        meng.luo\@majorbio.com;
 Script:			$Script
 Description:
-	fq thanslate to fa format
-	eg:
-	perl $Script -i -o -k -c
+
+	eg: perl -int filename -out filename 
+	
 
 Usage:
   Options:
-	"i:s"=>\$fin,                                                                                                                                                                        
-    "o:s"=>\$fout,
-	"m:s"=>\$min,
-  -h         Help
+	-int input file name
+	-out ouput file name 
+	-h         Help
 
 USAGE
         print $usage;
