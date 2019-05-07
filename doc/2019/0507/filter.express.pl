67329b98300f42cfb6fe477efff2ababfcdd7d3c
Chenzhe Luo <czheluo@gmail.com>
2019-05-07 16:39:32 +0800

    get normal express value
    get normal express value

---
+++ doc/2019/0507/filter.express.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+my $BEGIN_TIME=time();
+use Getopt::Long;
+my ($fin,$fout,$min);
+use Data::Dumper;
+use FindBin qw($Bin $Script);
+use File::Basename qw(basename dirname);
+my $version="1.0.0";
+GetOptions(
+	"help|?" =>\&USAGE,
+	"int:s"=>\$fin,
+	"out:s"=>\$fout,
+			) or &USAGE;
+&USAGE unless ($fout);
+open In,$fin;
+open Out,">$fout";
+while (<In>) {
+	chomp;
+	if (/^seq_id/) {
+		print Out "$_\n";
+	}else{
+		my ($geneid,$ids)=split/\,/,$_,2;
+		my $n=0;
+		my @fpk=split(",",$ids);
+		#print Dumper @fpk;die;
+		foreach my $id (@fpk) {
+			if ($id < 1 ) {
+				$n++;
+			}
+		}
+		next if ($n >=0);
+		print Out "$_\n";
+	}
+}
+close In;
+close Out;
+
+#######################################################################################
+print STDOUT "\nDone. Total elapsed time : ",time()-$BEGIN_TIME,"s\n";
+#######################################################################################
+sub ABSOLUTE_DIR #$pavfile=&ABSOLUTE_DIR($pavfile);
+{
+	my $cur_dir=`pwd`;chomp($cur_dir);
+	my ($in)=@_;
+	my $return="";
+	if(-f $in){
+		my $dir=dirname($in);
+		my $file=basename($in);
+		chdir $dir;$dir=`pwd`;chomp $dir;
+		$return="$dir/$file";
+	}elsif(-d $in){
+		chdir $in;$return=`pwd`;chomp $return;
+	}else{
+		warn "Warning just for file and dir \n$in";
+		exit;
+	}
+	chdir $cur_dir;
+	return $return;
+}
+sub USAGE {#
+        my $usage=<<"USAGE";
+Contact:        meng.luo\@majorbio.com;
+Script:			$Script
+Description:
+
+	eg: perl -int filename -out filename 
+	
+
+Usage:
+  Options:
+	-int input file name
+	-out ouput file name 
+	-h         Help
+
+USAGE
+        print $usage;
+        exit;
+}
