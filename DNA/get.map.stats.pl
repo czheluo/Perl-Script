8709fb1c14d639f01a7e3300f90ffa523ff6bc8d
Meng Luo <czheluo@gmail.com>
2020-03-14 18:07:13 +0800

    mapping stats

---
+++ DNA/get.map.stats.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+my $BEGIN_TIME=time();
+use Getopt::Long;
+my ($fin,$fout);
+use Data::Dumper;
+use FindBin qw($Bin $Script);
+use File::Basename qw(basename dirname);
+my $version="1.0.0";
+GetOptions(
+	"help|?" =>\&USAGE,
+	"int:s"=>\$fin,
+	"out:s"=>\$fout,
+			) or &USAGE;
+&USAGE unless ($fout);
+open In,"paste $fin/*.result.stat|";
+open Out,">$fout/total.map.result.xls";
+while (<In>) {
+	chomp;
+	next if ($_ eq ""||/^$/);
+	my @a=split(/\t/,$_);
+	my @out;
+	push @out,$a[0];
+	for (my $i=1;$i<@a;$i+=2) {
+		push @out,$a[$i];
+	}
+	print Out join("\t",@out),"\n";
+}
+close Out;
+close In;
+
+
+
+#######################################################################################
+print STDOUT "\nDone. Total elapsed time : ",time()-$BEGIN_TIME,"s\n";
+#######################################################################################
+sub ABSOLUTE_DIR #$pavfile=&ABSOLUTE_DIR($pavfile);
+{
+	my $cur_dir=`pwd`;chomp($cur_dir);
+	my ($in)=@_;
+	my $return="";
+	if(-f $in){
+		my $dir=dirname($in);
+		my $file=basename($in);
+		chdir $dir;$dir=`pwd`;chomp $dir;
+		$return="$dir/$file";
+	}elsif(-d $in){
+		chdir $in;$return=`pwd`;chomp $return;
+	}else{
+		warn "Warning just for file and dir \n$in";
+		exit;
+	}
+	chdir $cur_dir;
+	return $return;
+}
+sub USAGE {#
+        my $usage=<<"USAGE";
+Contact:        meng.luo\@majorbio.com;
+Script:			$Script
+Description:
+
+	eg: perl -int filename -out filename 
+	
+Usage:
+  Options:
+	-int input file name
+	-out output file name 
+	-h         Help
+
+USAGE
+        print $usage;
+        exit;
+}
